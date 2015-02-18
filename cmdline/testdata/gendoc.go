4731b364d785b8c7c573121eba4c3d90b4cc029b
Suharsh Sivakumar <suharshs@google.com>
2015-02-17 21:01:37 -0800

    lib/cmdline/testdata/gendoc: Make gendoc build packages instead of running group of files.
    'go run ...' ignores build tags. So, when generating docs for main's that
    involve conditional builds, redifined variable errors were unavoidable.
    
    To fix this I modified gendoc to first build the package and then run
    the executable with the doc generation flags.
    
    https://vanadium-review.googlesource.com/3951 depends on this.
    
    Change-Id: I2678835a1b310b2306b6697a0a71e5a7fdb048f5

--- cmdline/testdata/gendoc.go
+++ cmdline/testdata/gendoc.go
@@ -28,54 +28,56 @@ import (
 )
 
 func main() {
-	if got, want := len(os.Args[1:]), 1; got < want {
-		fmt.Fprintln(os.Stderr, "gendoc requires at least one argument")
-		fmt.Fprintln(os.Stderr, "usage: gendoc <dir> [args]")
+	if err := generate(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
 
-	dir := os.Args[1]
+func generate() error {
+	if got, want := len(os.Args[1:]), 1; got < want {
+		return fmt.Errorf("gendoc requires at least one argument\nusage: gendoc <dir> [args]")
+	}
+	pkg := os.Args[1]
 
-	doc := `// This file was auto-generated via go generate.
+	// Build the gendoc binary in a temporary folder.
+	tmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		return fmt.Errorf("TempDir() failed: %v", err)
+	}
+	defer os.RemoveAll(tmpDir)
+	gendocBin := filepath.Join(tmpDir, "gendoc")
+	args := []string{"go", "build", "-o", gendocBin}
+	args = append(args, pkg)
+	buildCmd := exec.Command("v23", args...)
+	if err := buildCmd.Run(); err != nil {
+		return fmt.Errorf("%q failed: %v\n", strings.Join(buildCmd.Args, " "), err)
+	}
+
+	// Use it to generate the documentation.
+	var out bytes.Buffer
+	if len(os.Args) == 2 {
+		args = []string{"help", "-style=godoc", "..."}
+	} else {
+		args = os.Args[2:]
+	}
+	runCmd := exec.Command(gendocBin, args...)
+	runCmd.Stdout = &out
+	if err := runCmd.Run(); err != nil {
+		return fmt.Errorf("%q failed: %v\n%v\n", strings.Join(runCmd.Args, " "), err)
+	}
+	doc := fmt.Sprintf(`// This file was auto-generated via go generate.
 // DO NOT UPDATE MANUALLY
 
 /*
-`
-	fileInfoList, err := ioutil.ReadDir(dir)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "ReadDir(%v) failed: %v\n", dir, err)
-		os.Exit(1)
-	}
-	goFiles := []string{}
-	for _, fileInfo := range fileInfoList {
-		if !fileInfo.Mode().IsRegular() {
-			continue
-		}
-		if strings.HasSuffix(fileInfo.Name(), ".go") && !strings.HasSuffix(fileInfo.Name(), "_test.go") {
-			goFiles = append(goFiles, fileInfo.Name())
-		}
-	}
-	var out bytes.Buffer
-	args := []string{"go", "run"}
-	args = append(args, goFiles...)
-	if len(os.Args) == 2 {
-		args = append(args, "help", "-style=godoc", "...")
-	} else {
-		args = append(args, os.Args[2:]...)
-	}
-	runCmd := exec.Command("v23", args...)
-	runCmd.Stdout = &out
-	if err := runCmd.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "%q failed: %v\n%v\n", strings.Join(runCmd.Args, " "), out.String(), err)
-		os.Exit(1)
-	}
-	doc += out.String()
-	doc += `*/
+%s*/
 package main
-`
-	path, perm := filepath.Join(dir, "doc.go"), os.FileMode(0644)
+`, out.String())
+
+	// Write the result to doc.go.
+	path, perm := filepath.Join(pkg, "doc.go"), os.FileMode(0644)
 	if err := ioutil.WriteFile(path, []byte(doc), perm); err != nil {
-		fmt.Fprintf(os.Stderr, "WriteFile(%v, %v) failed: %v\n", path, perm, err)
-		os.Exit(1)
+		return fmt.Errorf("WriteFile(%v, %v) failed: %v\n", path, perm, err)
 	}
+	return nil
 }
