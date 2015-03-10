2b786999a5bc8647d9c533599e03c7a3f7c4a422
Cosmos Nicolaou <cnicolaou@google.com>
2015-03-10 07:48:50 -0700

    modules usability improvements: https://vanadium-review.googlesource.com/#/c/7060/ MultiPart: 3/5
    Change-Id: Ibb8a96d96ba7ee0a5861a182d4b191f176e56924

--- vlog/v23_test.go
+++ vlog/v23_test.go
@@ -19,7 +19,7 @@ func init() {
 
 func TestMain(m *testing.M) {
 	testutil.Init()
-	if modules.IsModulesProcess() {
+	if modules.IsModulesChildProcess() {
 		if err := modules.Dispatch(); err != nil {
 			fmt.Fprintf(os.Stderr, "modules.Dispatch failed: %v\n", err)
 			os.Exit(1)
