e0d76698583610530dcc7933b54e83383ff43f6e
Jiri Simsa <jsimsa@google.com>
2015-03-11 18:36:39 -0700

    cmdline: add support for command-line flags that contain variables
    MultiPart: 1/3
    
    Change-Id: I6b46a286219d20f997bb6d90b602b7caeea8a216

---
+++ cmdline/testdata/flag.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"v.io/x/lib/cmdline"
+)
+
+var (
+	testFlag flag.Getter
+)
+
+func main() {
+	os.Exit(root().Main())
+}
+
+func init() {
+	os.Setenv("TEST", "HELLO")
+	testFlag = cmdline.EnvFlag("${TEST}")
+	cmdRoot.Flags.Var(testFlag, "test", "test flag")
+}
+
+// root returns a command that represents the root of the v23 tool.
+func root() *cmdline.Command {
+	return cmdRoot
+}
+
+// cmdRoot represents the root of the test command.
+var cmdRoot = &cmdline.Command{
+	Run:   runRoot,
+	Name:  "test",
+	Short: "test command",
+	Long:  "test command.",
+}
+
+func runRoot(*cmdline.Command, []string) error {
+	if got, want := testFlag.Get().(string), "HELLO"; got != want {
+		return fmt.Errorf("unexpected value: got %v, want %v", got, want)
+	}
+	return nil
+}
