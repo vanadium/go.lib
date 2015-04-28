ebf8cfa7f834e2e9e7482956b1ce4602223ad188
Jiri Simsa <jsimsa@google.com>
2015-04-27 17:35:01 -0700

    lib: support for setting manifest and pristine flag at build-time
    This multi-part CL consists of related changes:
    
    1) The v.io/x/devtools/internal/util package has been modified to
    record the "current manifest", which identifies thatstate of v23
    projects resulting from the latest invocation of "v23 update".
    
    2) The v.io/x/lib/buildinfo and v.io/x/devtools/v23 have been modified
    to record the current manifest information inside of the binaries that
    are built using "v23 go build" or "v23 go install".
    
    3) In addition to recording the current manifest, this CL also records
    a boolean indicator of whether a binary has been built from a pristine
    state (i.e. all projects are on a master branch and there are no
    uncommitted changes or untracked files).
    
    MultiPart: 2/3
    
    Change-Id: Id5326d2bd7fe446b2ac42f765f50f992682deeb8

--- buildinfo/buildinfo.go
+++ buildinfo/buildinfo.go
@@ -2,32 +2,71 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package buildinfo implements a mechanism for injecting build-time metadata
-// into executable binaries.
+// Package buildinfo implements a mechanism for injecting build-time
+// metadata into executable binaries.
 package buildinfo
 
 import (
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"runtime"
+	"strconv"
+	"time"
 )
 
-// These variables are filled in at link time, using:
-//  -ldflags "-X v.io/x/lib/buildinfo.<varname> <value>"
-var timestamp, username, platform string
+var (
+	manifest  string
+	platform  string
+	pristine  string
+	timestamp string
+	username  string
+)
 
 // T describes binary metadata.
 type T struct {
-	GoVersion, BuildTimestamp, BuildUser, BuildPlatform string
+	// BuildPlatform records the target platform of the build.
+	BuildPlatform string
+	// BuildTimestamp records the time of the build.
+	BuildTimestamp time.Time
+	// BuildUser records the name of user who executed the build.
+	BuildUser string
+	// GoVersion records the Go version used for the build.
+	GoVersion string
+	// Manifest records the project manifest that identifies the state
+	// of Vanadium projects used for the build.
+	Manifest string
+	// Pristine records whether the build was executed using pristine
+	// master branches of Vanadium projects (or not).
+	Pristine bool
 }
 
 var info T
 
 func init() {
-	info = T{
-		GoVersion:      runtime.Version(),
-		BuildTimestamp: timestamp,
-		BuildUser:      username,
-		BuildPlatform:  platform,
+	info.BuildPlatform = platform
+	if timestamp != "" {
+		var err error
+		info.BuildTimestamp, err = time.Parse(time.RFC3339, timestamp)
+		if err != nil {
+			panic(fmt.Sprintf("Parse(%v) failed: %v", timestamp, err))
+		}
+	}
+	info.BuildUser = username
+	info.GoVersion = runtime.Version()
+	if manifest != "" {
+		decodedBytes, err := base64.StdEncoding.DecodeString(manifest)
+		if err != nil {
+			panic(fmt.Sprintf("DecodeString() failed: %v", err))
+		}
+		info.Manifest = string(decodedBytes)
+	}
+	if pristine != "" {
+		b, err := strconv.ParseBool(pristine)
+		if err != nil {
+			panic(fmt.Sprintf("ParseBool(%v) failed: %v", pristine, err))
+		}
+		info.Pristine = b
 	}
 }
 
