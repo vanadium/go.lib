af3debe7ee116fc5c2f49acd0f5e9f2f07519c1d
Cosmos Nicolaou <cnicolaou@google.com>
2015-05-27 21:43:26 -0700

    lib/vlog: remove the LogCall functions that are now in x/ref/lib/apilog.
    Change-Id: Ib93d79f8a27ab4e3fabe2c440428ef3f597c9f7e

--- vlog/log_test.go
+++ vlog/log_test.go
@@ -125,54 +125,6 @@ func TestDepth(t *testing.T) {
 		}
 	}
 }
-
-func myLoggedFunc() {
-	f := vlog.LogCall("entry")
-	f("exit")
-}
-
-func TestLogCall(t *testing.T) {
-	dir, err := ioutil.TempDir("", "logtest")
-	defer os.RemoveAll(dir)
-	if err != nil {
-		t.Fatalf("unexpected error: %s", err)
-	}
-	logger := vlog.NewLogger("testHeader")
-	logger.Configure(vlog.LogDir(dir), vlog.Level(2))
-	saveLog := vlog.Log
-	defer vlog.SetLog(saveLog)
-	vlog.SetLog(logger)
-
-	myLoggedFunc()
-	vlog.FlushLog()
-	contents, err := readLogFiles(dir)
-	if err != nil {
-		t.Fatalf("unexpected error: %s", err)
-	}
-	logCallLineRE := regexp.MustCompile(`\S+ \S+\s+\S+ ([^:]*):.*(call|return)\[(\S*)`)
-	for _, line := range contents {
-		match := logCallLineRE.FindStringSubmatch(line)
-		if len(match) != 4 {
-			t.Errorf("failed to match %s", line)
-			continue
-		}
-		fileName, callType, funcName := match[1], match[2], match[3]
-		if fileName != "log_test.go" {
-			t.Errorf("unexpected file name: %s", fileName)
-			continue
-		}
-		if callType != "call" && callType != "return" {
-			t.Errorf("unexpected call type: %s", callType)
-		}
-		if funcName != "vlog_test.myLoggedFunc" {
-			t.Errorf("unexpected func name: %s", funcName)
-		}
-	}
-	if want, got := 2, len(contents); want != got {
-		t.Errorf("Expected %d info lines, got %d instead", want, got)
-	}
-}
-
 func TestVModule(t *testing.T) {
 	dir, err := ioutil.TempDir("", "logtest")
 	defer os.RemoveAll(dir)
