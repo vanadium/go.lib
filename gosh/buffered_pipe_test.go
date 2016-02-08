3c83e283916f047772056d7b5cd84a8513890bf3
Bogdan Caprita <caprita@google.com>
2016-02-08 09:27:14 -0800

    TBR: gosh: revert Shyam's roll-back of Todd's WriteTo/ReadFrom change:
         Revert "lib: Revert https://vanadium-review.googlesource.com/#/c/19347/"
    
    A dependent cl will fix the issue with the implementation of WriteTo
    that was causing lib/signals_test to hang.
    
    Change-Id: I94a96e266cd46125b031e095a00fd8a0b1e82286

--- gosh/buffered_pipe_test.go
+++ gosh/buffered_pipe_test.go
@@ -5,23 +5,57 @@
 package gosh
 
 import (
+	"bytes"
+	"io"
 	"io/ioutil"
+	"strings"
 	"testing"
 )
 
-func TestReadAfterClose(t *testing.T) {
+func TestReadWriteAfterClose(t *testing.T) {
 	p := newBufferedPipe()
-	if _, err := p.Write([]byte("foo")); err != nil {
-		t.Errorf("write failed: %v", err)
+	if n, err := p.Write([]byte("foo")); n != 3 || err != nil {
+		t.Errorf("write got (%v,%v) want (3,nil)", n, err)
+	}
+	if n, err := p.Write([]byte("barbaz")); n != 6 || err != nil {
+		t.Errorf("write got (%v,%v) want (6,nil)", n, err)
 	}
 	if err := p.Close(); err != nil {
 		t.Errorf("close failed: %v", err)
 	}
-	b, err := ioutil.ReadAll(p)
-	if err != nil {
-		t.Errorf("read failed: %v", err)
+	// Read after close returns all data terminated by EOF.
+	if b, err := ioutil.ReadAll(p); string(b) != "foobarbaz" || err != nil {
+		t.Errorf("read got (%s,%v) want (foobarbaz,nil)", b, err)
 	}
-	if got, want := string(b), "foo"; got != want {
-		t.Errorf("got %s, want %s", got, want)
+	// Write after close fails.
+	n, err := p.Write([]byte("already closed"))
+	if got, want := n, 0; got != want {
+		t.Errorf("write after close got n %v, want %v", got, want)
+	}
+	if got, want := err, io.ErrClosedPipe; got != want {
+		t.Errorf("write after close got error %v, want %v", got, want)
+	}
+}
+
+func TestReadFromWriteTo(t *testing.T) {
+	p, buf := newBufferedPipe(), new(bytes.Buffer)
+	if n, err := p.(io.ReaderFrom).ReadFrom(strings.NewReader("foobarbaz")); n != 9 || err != nil {
+		t.Errorf("readfrom got (%v,%v) want (9,nil)", n, err)
+	}
+	if n, err := p.(io.WriterTo).WriteTo(buf); n != 9 || err != nil {
+		t.Errorf("writeto got (%v,%v) want (9,nil)", n, err)
+	}
+	if got, want := buf.String(), "foobarbaz"; got != want {
+		t.Errorf("writeto got %v want %v", got, want)
+	}
+	buf.Reset()
+	if n, err := p.(io.ReaderFrom).ReadFrom(strings.NewReader("foobarbaz")); n != 9 || err != nil {
+		t.Errorf("readfrom got (%v,%v) want (9,nil)", n, err)
+	}
+	if err := p.Close(); err != nil {
+		t.Errorf("close failed: %v", err)
+	}
+	if n, err := p.(io.WriterTo).WriteTo(buf); n != 9 || err != nil {
+		t.Errorf("writeto got (%v,%v) want (9,nil)", n, err)
 	}
 }
