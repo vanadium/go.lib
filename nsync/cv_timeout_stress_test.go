2c0da90296ac93a223ad26611999156c6f3f3012
Cosmos Nicolaou <cnicolaou@v.io>
2018-10-15 19:00:07 -0700

    fix flaky test

--- nsync/cv_timeout_stress_test.go
+++ nsync/cv_timeout_stress_test.go
@@ -145,10 +145,10 @@ func TestCVTimeoutStress(t *testing.T) {
 
 	// Some timeouts shoud have happened while the counts were being incremented.
 	expectedTimeouts = timeoutsSeen + 1000
-	if timeTaken > 5*time.Minute {
-		overTime := timeTaken - (5 * time.Minute)
-		expectedTimeouts -= 250 * uint64(overTime/time.Minute)
-		t.Logf("%v minutes of overtime, adjusting expeting timeouts accordingly", overTime)
+	if timeTaken > 5*time.Second {
+		overTime := timeTaken - (5 * time.Second)
+		expectedTimeouts -= 250 * uint64(overTime/time.Second)
+		t.Logf("%v of overtime, adjusting expeting timeouts accordingly", overTime)
 	}
 	if s.timeouts < expectedTimeouts {
 		t.Errorf("expected more than %d timeouts, got %d", expectedTimeouts, s.timeouts)
