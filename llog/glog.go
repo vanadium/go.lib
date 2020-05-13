156766e4eb1b8e01b288aeb25593a0d75e1cfa4f
Cosmos Nicolaou <cosmos.nicolaou@gmail.com>
2020-05-13 16:09:36 -0700

    glog: lint fixes

--- llog/glog.go
+++ llog/glog.go
@@ -1203,13 +1203,13 @@ func (lb logBridge) Write(b []byte) (n int, err error) {
 	buf := lb.log.getBuffer()
 	// Split "d.go:23: message" into "d.go", "23", and "message".
 	if parts := bytes.SplitN(b, []byte{':'}, 3); len(parts) != 3 || len(parts[0]) < 1 || len(parts[2]) < 1 {
-		fmt.Fprint(buf, fmt.Sprintf("bad log format: %s", b))
+		fmt.Fprintf(buf, "bad log format: %s", b)
 	} else {
 		file = string(parts[0])
 		line, err = strconv.Atoi(string(parts[1]))
 		fmt.Fprint(buf, string(b))
 		if err != nil {
-			fmt.Fprint(buf, fmt.Sprintf("bad line number: %s", b))
+			fmt.Fprintf(buf, "bad line number: %s", b)
 			line = 1
 		}
 	}
